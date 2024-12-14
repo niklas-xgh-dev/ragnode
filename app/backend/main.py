6e11403a23d8d711e7fc4fa94aa05244c441fc33
niklas-xgh-dev <techstag@outlook.com>
2024-12-14 22:48:45 +0100

    ci/cd test v1.01

--- app/backend/main.py
+++ app/backend/main.py
@@ -5,6 +5,6 @@ app=FastAPI()
 
 @app.get("/api/health")
 async def health_check():
-    return {"status": "healthy app v1.009"}
+    return {"status": "healthy app v1.01"}
 
 app.mount("/", StaticFiles(directory="../frontend", html=True))
