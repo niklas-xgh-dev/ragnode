d1927085da08f4539d89a481abb759e3b278d673
niklas-xgh-dev <techstag@outlook.com>
2025-01-19 23:13:42 +0100

    feature: implemented three chatbots, landing page and navbar with styling

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 import gradio as gr
 import os
 from dotenv import load_dotenv
@@ -23,9 +25,50 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Create chat interface
+# Configure templates
+templates = Jinja2Templates(directory="app/static")
+
+# Create a single chat interface that we'll reuse
+# Later we can modify ChatInterface to handle different environments
 chat_interface = ChatInterface().create_interface()
 
-# Mount Gradio app and static files
+# Mount the same Gradio app at different paths
 app = gr.mount_gradio_app(app, chat_interface, path="/chat/")
-app.mount("/", StaticFiles(directory="app/static", html=True), name="static")
+app = gr.mount_gradio_app(app, chat_interface, path="/diamond-hands/")
+app = gr.mount_gradio_app(app, chat_interface, path="/chippie/")
+app = gr.mount_gradio_app(app, chat_interface, path="/badener/")
+
+# Define routes for HTML pages
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/diamond-hands-chat", response_class=HTMLResponse)
+async def diamond_hands_page(request: Request):
+    return templates.TemplateResponse("chat-template.html", {
+        "request": request,
+        "title": "Diamond Hands",
+        "description": "Apes together stronk. Cools you down and doesn't let you YOLO stockpick. Based ETF recommendations.",
+        "chat_path": "/diamond-hands/"
+    })
+
+@app.get("/chippie-chat", response_class=HTMLResponse)
+async def chippie_page(request: Request):
+    return templates.TemplateResponse("chat-template.html", {
+        "request": request,
+        "title": "Chippie",
+        "description": "Semiconductor nerd. Takes one to like one.",
+        "chat_path": "/chippie/"
+    })
+
+@app.get("/badener-chat", response_class=HTMLResponse)
+async def badener_page(request: Request):
+    return templates.TemplateResponse("chat-template.html", {
+        "request": request,
+        "title": "The Badener",
+        "description": "Complete Baden fanboy, but at least knows everything there is about Baden.",
+        "chat_path": "/badener/"
+    })
+
+# Mount static files last to not override routes
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
